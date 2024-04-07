ad9f31e6bd8fd1f7bbea548b026541f434091d08
Lukasz Matyasik <lukaszpiotr.matyasik@gmail.com>
2024-04-07 16:41:38 +0200

    Implemented RPC

--- src/main/java/Send.java
+++ src/main/java/Send.java
@@ -1,16 +1,77 @@
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 // TODO: Classes to be exported as a Docker image in the future. This represents a node
 public class Send {
+    private static Connection establishConnection() throws Exception {
+        // TODO Read credential from a file
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("rat.rmq2.cloudamqp.com");
+        factory.setPort(5672);
+        factory.setUsername("sgbdexna");
+        factory.setVirtualHost("sgbdexna");
+        factory.setPassword("6JG_KIUYfXlkjSFU1lvov_G1ePwxZ9x0");
+        return factory.newConnection();
+    }
 
-    private final static String QUEUE_NAME = "join";
+    public void joinClient(Integer argAsInt) throws Exception {
+        final String QUEUE_NAME = "join";
+
+        String message = Integer.toString(argAsInt);
+
+        try (Connection connection = establishConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Request for node sent " + message);
+        } catch (RuntimeException e) {
+            System.out.println(" [.] " + e);
+        }
+    }
+
+    public String computeRoute(String message) throws IOException, InterruptedException, ExecutionException, Exception {
+        String requestQueueName = "rpc_queue";
+        Connection connection = establishConnection();
+        Channel channel = connection.createChannel();
+
+        final String corrId = UUID.randomUUID().toString();
+
+        String replyQueueName = channel.queueDeclare().getQueue();
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish("", requestQueueName, props, message.getBytes("UTF-8"));
+
+        final CompletableFuture<String> response = new CompletableFuture<>();
+
+        String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response.complete(new String(delivery.getBody(), "UTF-8"));
+            }
+        }, consumerTag -> {
+        });
+
+        String result = response.get();
+        channel.basicCancel(ctag);
+        return result;
+    }
 
     public static void main(String[] args) throws Exception {
 
+        Send send = new Send();
 
         int argAsInt = 0;
         if (args.length != 1) {
@@ -20,24 +81,25 @@ public class Send {
             try {
                 argAsInt = Integer.parseInt(args[0]);
                 System.out.println("Born of node number " + argAsInt);
+                send.joinClient(argAsInt);
             } catch (NumberFormatException e) {
                 System.out.println("Not an integer");
             }
         }
 
-        String message = Integer.toString(argAsInt);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write direction (r/l) and the node to compute the route ex. r 1 ");
+        while (true) {
+            String input = scanner.nextLine();
 
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("rat.rmq2.cloudamqp.com");
-        factory.setPort(5672);
-        factory.setUsername("sgbdexna");
-        factory.setVirtualHost("sgbdexna");
-        factory.setPassword("6JG_KIUYfXlkjSFU1lvov_G1ePwxZ9x0");
-        System.out.println("Trying to joing node... ");
-        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-            System.out.println(" [x] Request for node sent " + message);
+            try {
+                System.out.println(" [x] Requesting route for(" + input + ")");
+                String response = send.computeRoute(input);
+                System.out.println(" [.] Got '" + response + "'");
+
+            } catch (IOException | TimeoutException | InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/core/ServerSocket.java
+++ src/main/java/core/ServerSocket.java
@@ -1,25 +1,30 @@
 package core;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.*;
 
 public class ServerSocket {
+    static OverlayGraph overlay;
 
     private final static String QUEUE_NAME = "join";
 
-    public static void main(String[] args) throws Exception {
-
-        OverlayGraph overlay = new OverlayGraph();
-
+    private static Connection establishConnection() throws Exception {
+        // TODO Read credential from a file
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("rat.rmq2.cloudamqp.com");
         factory.setPort(5672);
         factory.setUsername("sgbdexna");
         factory.setVirtualHost("sgbdexna");
         factory.setPassword("6JG_KIUYfXlkjSFU1lvov_G1ePwxZ9x0");
-        Connection connection = factory.newConnection();
+        return factory.newConnection();
+    }
+
+
+    public void join() throws Exception {
+        String QUEUE_NAME = "join";
+
+        OverlayGraph overlay = new OverlayGraph();
+
+        Connection connection = establishConnection();
         Channel channel = connection.createChannel();
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
@@ -38,6 +43,56 @@ public class ServerSocket {
         });
 
         // TODO: Will each node have its own assigned queue for communication / RPC?
+    }
+
+    public void awaitRPC() throws Exception {
+
+        String RPC_QUEUE_NAME = "rpc_queue";
+
+        Connection connection = establishConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+        channel.queuePurge(RPC_QUEUE_NAME);
+
+        channel.basicQos(1); // number of server process
+
+        System.out.println(" [x] Awaiting RPC requests");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                    .Builder()
+                    .correlationId(delivery.getProperties().getCorrelationId())
+                    .build();
+
+            String response = "Example of computed answer";
+            try {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println("Receive following route request:" + message);
+            } catch (RuntimeException e) {
+                System.out.println(" [.] " + e);
+            } finally {
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes("UTF-8"));
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+
+        channel.basicConsume(RPC_QUEUE_NAME, false, deliverCallback, (consumerTag -> {
+        }));
+    }
+
+
+    public static void main(String main[]) {
+
+        ServerSocket server = new ServerSocket();
+        overlay = new OverlayGraph();
+
+        try {
+            server.join();
+            server.awaitRPC();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
 
