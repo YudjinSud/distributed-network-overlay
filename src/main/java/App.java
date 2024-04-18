1aa7437ab48659f650832cf65125a8061c95aaa8
Evgenii Sudakov <evgenii.sudakov@etu.univ-grenoble-alpes.fr>
2024-04-18 22:05:14 +0200

    start controller

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,25 @@
+import Conroller.AppController;
 import GUI.GUI;
 import core.NetworkGraph;
+import core.ServerSocket;
 
 public class App {
     public static void main(String[] args) {
-        GUI gui = new GUI();
 
-        gui.main(args);
+
+        try {
+
+//        GUI gui = new GUI();
+
+//        gui.main(args);
+
+            ServerSocket server = new ServerSocket();
+            server.main(args);
+
+            AppController appController = new AppController(server.overlay);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
---
+++ src/main/java/Conroller/AppController.java
@@ -0,0 +1,23 @@
+package Conroller;
+
+import GUI.GUI;
+import core.OverlayGraph;
+
+public class AppController {
+
+    private OverlayGraph overlayGraph;
+    private GUI graphView;
+
+    public Integer onAddNode(Integer nodeNb) {
+        System.out.println(nodeNb);
+
+        return nodeNb;
+    }
+
+    public AppController( OverlayGraph overlayGraph) {
+        this.overlayGraph = overlayGraph;
+//        this.graphView = graphView;
+
+        this.overlayGraph.setOnAddNodeCallback(this::onAddNode);
+    }
+}
